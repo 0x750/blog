6208959890647e5c500a24ada72095afbba26d22
Benoit Tieppo <benoit.tieppo@gmail.com>
2021-04-24 13:17:51 +0200

    feat: responsive mobile header

--- components/Header.js
+++ components/Header.js
@@ -1,19 +1,23 @@
+import { useState } from 'react';
+
 import Link from 'next/link';
 import styles from '../styles/components/Header.module.css';
 
 import { useSpring, animated } from 'react-spring'
 
 const Header = () => {
-    
-    const itemSpringStyles = [0, 600, 800, 1000].map((v) => 
-      useSpring({
-        from: { opacity: 0 },
-        to: { opacity: 1 },
-        delay: v,
-        config: {
-            duration: 500,
-        },
-      })
+
+    const [menuIsOpened, setMenuIsOpened] = useState(false);
+
+    const itemSpringStyles = [0, 600, 800, 1000].map((v) =>
+        useSpring({
+            from: { opacity: 0 },
+            to: { opacity: 1 },
+            delay: v,
+            config: {
+                duration: 500,
+            },
+        })
     );
 
     return (
@@ -37,7 +41,31 @@ const Header = () => {
                 <animated.div style={itemSpringStyles[3]}>
                     <span className={styles.menuitems}><a href="mailto:contact@benoit.fi">mail</a></span>
                 </animated.div>
+                <animated.div style={itemSpringStyles[1]}>
+                    <div onClick={() => setMenuIsOpened(!menuIsOpened)} className={styles.hamburger}>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                </animated.div>
             </div>
+            {menuIsOpened &&
+                <div className={styles.mobileMenu}>
+                    <Link href="/">
+                        <div>
+                            <span className={styles.menuitems}>home</span>
+                        </div>
+                    </Link>
+                    <Link href="/archives">
+                        <div>
+                            <span className={styles.menuitems}>archives</span>
+                        </div>
+                    </Link>
+                    <div>
+                        <span className={styles.menuitems}><a href="mailto:contact@benoit.fi">mail</a></span>
+                    </div>
+                </div>
+            }
         </div>
     )
 }
