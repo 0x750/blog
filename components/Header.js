f4fd3854c139fe4f1cf4fc5d1297a9ce14fb1914
Benoit Tieppo <benoit.tieppo@gmail.com>
2021-04-24 06:47:54 +0200

    feat: header animation

--- components/Header.js
+++ components/Header.js
@@ -1,21 +1,42 @@
 import Link from 'next/link';
 import styles from '../styles/components/Header.module.css';
 
+import { useSpring, animated } from 'react-spring'
+
 const Header = () => {
+    
+    const itemSpringStyles = [0, 600, 800, 1000].map((v) => 
+      useSpring({
+        from: { opacity: 0 },
+        to: { opacity: 1 },
+        delay: v,
+        config: {
+            duration: 500,
+        },
+      })
+    );
 
     return (
         <div className={styles.navbar}>
             <div className={styles.container}>
                 <Link href="/">
-                    <span className={styles.logo}>B</span>
+                    <animated.div style={itemSpringStyles[0]}>
+                        <span className={styles.logo}>B</span>
+                    </animated.div>
                 </Link>
                 <Link href="/">
-                    <span className={styles.menuitems}>home</span>
+                    <animated.div style={itemSpringStyles[1]}>
+                        <span className={styles.menuitems}>home</span>
+                    </animated.div>
                 </Link>
                 <Link href="/archives">
-                    <span className={styles.menuitems}>archives</span>
+                    <animated.div style={itemSpringStyles[2]}>
+                        <span className={styles.menuitems}>archives</span>
+                    </animated.div>
                 </Link>
-                <span className={styles.menuitems}><a href="mailto:contact@benoit.fi">mail</a></span>
+                <animated.div style={itemSpringStyles[3]}>
+                    <span className={styles.menuitems}><a href="mailto:contact@benoit.fi">mail</a></span>
+                </animated.div>
             </div>
         </div>
     )
